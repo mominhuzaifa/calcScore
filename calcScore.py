2522984a5b4f24c30c806f23d40ca00d6a225d50
Momin Mohammed Huzaifa <75272398+mominhuzaifa@users.noreply.github.com>
2020-12-01 00:07:25 +0530

    Add files via upload

---
+++ calcScore.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# score predictor of cricket match
+
+def predictor():
+    totalOver = int(input('Enter total over of match: '))
+    currentOver = float(input('Enter current over of match: '))
+    currentScore = int(input('Enter current score of match: '))
+    runrate = currentScore/currentOver
+    calcScore = runrate * totalOver
+    print("\n")
+    print("------------------------")
+    print("Run rate is: ", round(runrate,2))
+    print("Predicted score: ", round(calcScore))
+    print("------------------------")
+
+predictor()
